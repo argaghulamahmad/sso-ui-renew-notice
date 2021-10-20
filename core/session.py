c82c3aff9ec94d95ab5b2de369f8b036a3ea7680
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2021-10-20 18:03:09 +0700

    update script

--- core/session.py
+++ core/session.py
@@ -15,21 +15,7 @@ class Session:
         load_dotenv()
 
         options = webdriver.ChromeOptions()
-        options.add_experimental_option('w3c', False)
-
-        options.add_experimental_option("useAutomationExtension", False)
-        options.add_argument('--disable-blink-features=AutomationControlled')
-        options.add_experimental_option('excludeSwitches', ['enable-automation'])
-
-        options.add_experimental_option("prefs", {"profile.default_content_setting_values.geolocation": 1})
-
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--disable-notifications")
         options.add_argument("--incognito")
-
-        options.add_argument("--lang=en-GB")
-
         options.headless = False
 
         # Getting the chromedriver from cache or download it from internet
@@ -37,33 +23,37 @@ class Session:
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         self.browser.set_window_size(1250, 750)
 
-        # clear the console based on the operating system you're using
-        os.system('cls' if os.name == 'nt' else 'clear')
-
     def login_sso(self, ):
         try:
             self.browser.get(self.SSO_LOGIN_URL)
 
             username_input_field = self.browser.find_element(By.ID, "username")
+            username_input_field.clear()
             sso_username = os.getenv("SSO_USERNAME")
             username_input_field.send_keys(sso_username)
+
             password_input_field = self.browser.find_element(By.ID, "password")
             password_input_field.clear()
             sso_password = os.getenv("SSO_PASSWORD")
             password_input_field.send_keys(sso_password)
+
+            xpath_submit_button = "(.//*[normalize-space(text()) and normalize-space(.)='Enter your username and " \
+                                  "password'])[ " \
+                                  "1]/following::button[1]"
             self.browser.find_element(By.XPATH,
-                                      "(.//*[normalize-space(text()) and normalize-space(.)='Enter your username and "
-                                      "password'])[ "
-                                      "1]/following::button[1]").click()
+                                      xpath_submit_button).click()
         except NoSuchElementException:
             self.browser.quit()
 
     def dashboard_ui(self, ):
         try:
             self.browser.get(self.CHANGE_PASSWORD)
-            list_info = self.browser.find_element(By.ID, "sparks-info")
-            for info in list_info:
-                print(info.text)
+            xpath_expired_days_left = "/html/body/div[2]/div[2]/div[1]/div/ul/li[2]/h5/span"
+            xpath_login_as = "/html/body/div[2]/div[2]/div[1]/div/ul/li[1]"
+            expired_days_left_element = self.browser.find_element(By.XPATH, xpath_expired_days_left)
+            login_as_element = self.browser.find_element(By.XPATH, xpath_login_as)
+            print(expired_days_left_element.text)
+            print(login_as_element.text)
         except NoSuchElementException:
             self.browser.quit()
 
